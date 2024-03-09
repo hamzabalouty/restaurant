c54d38585b7f7d02eaac3d1f63c3a1929ef00ec6
hamzaismail4 <hbalouty22@gmail.com>
2024-03-09 16:46:27 +0200

    update

--- src/data.js
+++ src/data.js
@@ -5,6 +5,17 @@ import Img4 from './assets/product-4-105x105.jpg';
 import Img5 from './assets/product-8-105x105.jpg';
 import Img6 from './assets/bbq-105x105.jpg';
 
+import Img7 from './assets/product-1-370x247.jpg';
+import Img8 from './assets/new-product-2-370x247.jpg';
+import Img9 from './assets/product-4-370x247.jpg';
+import Img10 from './assets/product-5-370x247.jpg';
+import Img11 from './assets/product-7-370x247.jpg';
+import Img12 from './assets/product-5-370x247.jpg';
+
+import Img13 from './assets/pizza-9-370x247.jpg';
+import Img14 from './assets/product-2-370x247.jpg';
+
+
 export const menuData = [
     {
         id: 1,
@@ -47,5 +58,216 @@ export const menuData = [
         title: 'Roasted Pork Belly',
         price: '13.50',
         desc: 'Pan roasted pork belly with gratin potato, braised Savoy cabbage, apples, thyme and calvados jus Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce Sautéed breaded veal.'
+    },
+]
+
+export const shopData = [
+    {
+        id: 1,
+        image: Img7,
+        category: 'SALADS',
+        price: '15.00',
+        title: 'Cherry Pie',
+        desc: 'Cherries, Dough, White Sugar, Cheese, Vanilla Ice Cream',
+    },
+    {
+        id: 2,
+        image: Img8,
+        category: 'SALADS',
+        price: '25.00',
+        title: 'Greek Salad',
+        desc: 'Tomatoes, Cucumber, Onion, Olives, Feta, Oregano, Romaine Lettuce.',
+    },
+    {
+        id: 3,
+        image: Img9,
+        category: 'SALADS',
+        price: '15.00',
+        title: 'Apple Strudel',
+        desc: 'Apples, Vanilla Ice Cream, Nuts',
+    },
+    {
+        id: 4,
+        image: Img10,
+        category: 'SALADS',
+        price: '30.00',
+        title: 'Greek Salad',
+        desc: 'Tuna, Red Onion, Caper, Mozzarella, Tomato Sauce'
+    },
+    {
+        id: 5,
+        image: Img11,
+        category: 'SALADS',
+        price: '21.00',
+        title: 'Fish Parmentier',
+        desc: 'Cod, haddock, prawn and salmon in a creamy white wine and leek sauce, topped with mash potato and Gruyère cheese'
+    },
+    {
+        id: 6,
+        image: Img12,
+        category: 'SALADS',
+        price: '30.00',
+        title: 'Caeser Salad',
+        desc: 'Lettuce, Eggs, Parmesan Cheese, Chicken Breast Fillets.',
+    },
+    {
+        id: 7,
+        image: Img7,
+        category: 'PIZZA',
+        price: '15.00',
+        title: 'Cherry Pie',
+        desc: 'Cherries, Dough, White Sugar, Cheese, Vanilla Ice Cream',
+    },
+    {
+        id: 8,
+        image: Img13,
+        category: 'PIZZA',
+        price: '15.00',
+        title: 'Pizza Margherita',
+        desc: 'Mushrooms, Pepperoni, Mozzarella, Tomato, Salami, Olive',
+    },
+    {
+        id: 9,
+        image: Img8,
+        category: 'PIZZA',
+        price: '25.00',
+        title: 'Greek Salad',
+        desc: 'Tomatoes, Cucumber, Onion, Olives, Feta, Oregano, Romaine Lettuce.',
+    },
+    {
+        id: 10,
+        image: Img9,
+        category: 'PIZZA',
+        price: '15.00',
+        title: 'Apple Strudel',
+        desc: 'Apples, Vanilla Ice Cream, Nuts',
+    },
+    {
+        id: 11,
+        image: Img14,
+        category: 'PIZZA',
+        price: '23.00',
+        title: 'Spinach Soup',
+        desc: 'Onion, Spinach, Pepper Powder, Milk, Salt, Leek',
+    },
+    {
+        id: 12,
+        image: Img12,
+        category: 'PIZZA',
+        price: '30.00',
+        title: 'Greek Salad',
+        desc: 'Tuna, Red Onion, Caper, Mozzarella, Tomato Sauce',
+    },
+    {
+        id: 13,
+        image: Img11,
+        category: 'PIZZA',
+        price: '21.00',
+        title: 'Fish Parmentier',
+        desc: 'Cod, haddock, prawn and salmon in a creamy white wine and leek sauce, topped with mash potato and Gruyère cheese',
+    },
+    {
+        id: 14,
+        image: Img10,
+        category: 'PIZZA',
+        price: '30.00',
+        title: 'Caeser Salad',
+        desc: 'Lettuce, Eggs, Parmesan Cheese, Chicken Breast Fillets.',
+    },
+    {
+        id: 15,
+        image: Img7,
+        category: 'SOUPS',
+        price: '15.00',
+        title: 'Cherry Pie',
+        desc: 'Cherries, Dough, White Sugar, Cheese, Vanilla Ice Cream'
+    },
+    {
+        id: 16,
+        image: Img13,
+        category: 'SOUPS',
+        price: '15.00',
+        title: 'Pizza Margherita',
+        desc: 'Mushrooms, Pepperoni, Mozzarella, Tomato, Salami, Olive'
+    },
+    {
+        id: 17,
+        image: Img8,
+        category: 'SOUPS',
+        price: '25.00',
+        title: 'Greek Salad',
+        desc: 'Tomatoes, Cucumber, Onion, Olives, Feta, Oregano, Romaine Lettuce.'
+    },
+    {
+        id: 18,
+        image: Img12,
+        category: 'SOUPS',
+        price: '30.00',
+        title: 'Greek Salad',
+        desc: 'Tuna, Red Onion, Caper, Mozzarella, Tomato Sauce'
+    },
+    {
+        id: 19,
+        image: Img12,
+        category: 'SOUPS',
+        price: '30.00',
+        title: 'Caeser Salad',
+        desc: 'Lettuce, Eggs, Parmesan Cheese, Chicken Breast Fillets.'
+    },
+    {
+        id: 20,
+        image: Img13,
+        category: 'DESSERTS',
+        price: '15.00',
+        title: 'Pizza Margherita',
+        desc: 'Mushrooms, Pepperoni, Mozzarella, Tomato, Salami, Olive'
+    },
+    {
+        id: 21,
+        image: Img9,
+        category: 'DESSERTS',
+        price: '15.00',
+        title: 'Apple Strudel',
+        desc: 'Apples, Vanilla Ice Cream, Nuts'
+    },
+    {
+        id: 22,
+        image: Img10,
+        category: 'DESSERTS',
+        price: '30.00',
+        title: 'Greek Salad',
+        desc: 'Tuna, Red Onion, Caper, Mozzarella, Tomato Sauce'
+    },
+    {
+        id: 23,
+        image: Img11,
+        category: 'DESSERTS',
+        price: '21.00',
+        title: 'Fish Parmentier',
+        desc: 'Cod, haddock, prawn and salmon in a creamy white wine and leek sauce, topped with mash potato and Gruyère cheese'
+    },
+    {
+        id: 24,
+        image: Img9,
+        category: 'DRINKS',
+        price: '15.00',
+        title: 'Apple Strudel',
+        desc: 'Apples, Vanilla Ice Cream, Nuts'
+    },
+    {
+        id: 25,
+        image: Img12,
+        category: 'DRINKS',
+        price: '30.00',
+        title: 'Greek Salad',
+        desc: 'Tuna, Red Onion, Caper, Mozzarella, Tomato Sauce'
+    },
+    {
+        id: 26,
+        image: Img11,
+        category: 'DRINKS',
+        price: '21.00',
+        title: 'Fish Parmentier',
+        desc: 'Cod, haddock, prawn and salmon in a creamy white wine and leek sauce, topped with mash potato and Gruyère cheese'
     },
 ]
--- src/pages/Shop/Shop.js
+++ src/pages/Shop/Shop.js
@@ -1,14 +1,73 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react';
+import './Shop.css';
+import { NavLink } from 'react-router-dom';
+import { shopData } from "../../data";
+
 
 function Shop() {
+
+  const [itemsData, setItemsData] = useState(shopData);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(8);
+
+  const lastPostIndex = currentPage * postsPerPage;
+  const firstPostIndex = lastPostIndex - postsPerPage;
+  const currentPosts = itemsData.slice(firstPostIndex, lastPostIndex);
+
+  //get all cat uniqe
+  const allCategory = [...new Set(shopData.map((i) => i.category))];
+
+  //filter by category
+  const filterbyCategory = (cat) => {
+    if (cat === "الكل") {
+      setItemsData(shopData);
+    } else {
+      const newArr = shopData.filter((item) => item.category === cat);
+      setItemsData(newArr);
+    }
+  };
+
+  //to filter by category
+  const onFilter = (cat) => {
+    filterbyCategory(cat);
+  };
 
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
   return (
-    <div>Shop</div>
+    <div className='shop'>
+      <div className='shop-title'>
+        <h3>Shop</h3>
+      </div>
+      <ul className="shop-cat-btns">
+        {allCategory.map((cat) => (
+          <li>
+            <NavLink className="navlink-btn" onClick={() => onFilter(cat)}>
+                {cat}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+      <div className='cards-container'>
+        {currentPosts.map((item) => (
+          <div className='card-box' key={item.id}>
+            <img src={item.image} alt='image'/>
+            <div className='price-overlay'>
+              <p>${item.price}</p>
+            </div>
+            <div className='card-content'>
+              <h4>{item.title}</h4>
+              <p>{item.desc}</p>
+              <button className='addtocart-card'>Add To Cart</button>
+            </div>
+          </div>
+        ))}
+          
+      </div>
+    </div>
   )
 }
 
-export default Shop
+export default Shop;
